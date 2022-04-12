a8ea9850c8c493b4b0ddc7bdb420edb986bfaa92
sina taherkhani <56226543+sinath72@users.noreply.github.com>
2022-04-12 16:01:27 +0430

    Update Transaction_List.swift

--- Sources/IDPay_Gateway_Payments/IDPay/View/Transaction_List.swift
+++ Sources/IDPay_Gateway_Payments/IDPay/View/Transaction_List.swift
@@ -26,7 +26,7 @@ public struct ListTransaction {
                 let statusCode = (response as! HTTPURLResponse).statusCode
                 print("URL Session Task Succeeded: HTTP \(statusCode)")
                 let responseString = try? JSON(data: data!)
-                print(responseString!["records"][0])
+                print(responseString!["records"][68])
             }
             else {
                 // Failure
