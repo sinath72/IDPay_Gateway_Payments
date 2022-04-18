0a6b5391bae68ffd7323e41194ad8f9bff8b23cf
sina taherkhani <56226543+sinath72@users.noreply.github.com>
2022-04-18 15:31:18 +0430

    Update Payment.swift

--- Sources/IDPay_Gateway_Payments/IDPay/View/Payment.swift
+++ Sources/IDPay_Gateway_Payments/IDPay/View/Payment.swift
@@ -16,8 +16,7 @@ public struct payment{
     public weak var deleget:DataModelDelegate?
     public init(){
     }
-    /* مقادیر api_key, callbackURL, order_id, amount ضروری میباشند و مقادیر دیگر بصورت دلخواه میتوان تنظیم کرد یا اصلا در تابع وجود نداشته باشند بصورت:     getPay(api_key:self.apikey,callbackURL:self.callbackurl,order_id:self.orderid,amount:self.amount) باشد و مقادیر دیگر را بصورت دلخواه میتوان اضافه کرد که: name,phone,mail,description می باشند
-     */
+    /* the parameters are necessare is: api_key, callbackURL, order_id, amount and other parameters of this: name,phone,mail,description is optional parameters  */
     public func getPay(api_key:String,callbackURL:String,order_id:String,amount:Int,name:String = "",phone:String = "",mail:String = "",description:String = ""){
         let sessionConfig = URLSessionConfiguration.default
         let session = URLSession(configuration: sessionConfig)
